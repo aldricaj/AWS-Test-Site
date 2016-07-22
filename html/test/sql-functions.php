b6da079d6f87f0f38f5bdae8c3df14bb1be659b6
aldricaj <aldricaj@mail.uc.edu>
2016-07-22 17:18:55 +0000

    Moved SQL functions into seperate file

---
+++ html/test/sql-functions.php
@@ -0,0 +1,45 @@
+/*
+ * This file holds several php functions for acessing the agency assignments
+ */
+
+<?php
+    /*
+     *  This function querys the database using the passed query 
+     *  and returns the resulting table
+     */
+    function queryDatabase($queryText){
+        // establish connection to database
+        $servername = "localhost";
+        $usr = "root";
+        $pass_file=fopen(".mysql-pass","r");
+        $pass = trim(fgets($pass_file));
+        $db_name = "agency_assignments";
+        $db_conn = new mysqli($servername,$usr,$pass,$db_name);
+        fclose($pass_file);
+        return $db_conn->query($queryText);
+    }
+    /*
+     * This function will query for the departments and return an array
+     */
+    function getAgencies(){
+        $agency_query = "SELECT `Agency` FROM `Assignments` WHERE 1";
+        $agency_table = queryDatabase($agency_query);
+        $index = 0;
+        $agency_list = array();
+        while($curr_row = $agency_table->fetch_assoc()){
+            $agency_list[] = $curr_row["Agency"];
+            $index++;
+        }
+        return $agency_list;
+    }
+    
+    function getContacts($agency){
+        // ensure that the search term is in all caps
+        $agency = strtoupper($agency);
+        $contact_query = "SELECT `GM/CM*`, `AD**` FROM `Assignments`
+                            WHERE UPPER(`Agency`) = \"".$agency ."\"";
+        $contact_table = queryDatabase($contact_query);
+        $result = $contact_table->fetch_assoc();
+        return $result;
+    }
+?>
--- html/test/update.php
+++ html/test/update.php
@@ -1,13 +1,30 @@
-/*
- *  This page will update a database entry for an agency
- */
 <!Doctype html>
 <html>
     <body>
         <h3>Update Contact</h3>
-        <?php
-            // define quality of life functions
-            // 
-        ?>
+<?php
+/*
+ *  This page will update a database entry for an agency
+ */
+
+        include 'sql-functions.php';
+        // set up html
+        echo "<p> Select the agency you want to find the contact info for below</p>";
+        // create input form
+        $agencies = getAgencies();
+        echo "<form action='update.php'>";
+        echo "Agency Name: <br/>";
+        echo "<select name='agency_select'>";
+        echo "<option selected>Choose a department</option>";
+        
+        // output the options to the html
+        for ($index = 0;$index < count($agencies); $index++){
+            echo "<option >".strval($agencies[$index])."</option>";
+        }
+        echo "</select>";
+        // create the submit button
+        echo "<input type='submit' value='Search'><br/>";
+        echo "</input>";
+?>
     </body>
 </html>
